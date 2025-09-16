3877e5427595ff305a6a20f34847892a737d25c4
jesperxu <jesperxu@tencent.com>
2025-09-16 14:38:24 +0800

    post
    ;
    ;
    ;

--- edge-functions/index.js
+++ edge-functions/index.js
@@ -1,8 +1,8 @@
-export function onRequest(context) {
+export function onRequestPost(context) {
     console.log(context)
     const { request } = context;
     const { url, method } = request;
-    const res = new Response(`${method} ${url}`, {
+    const res = new Response(`${method} ${url} ${context}`, {
         status: 200,
         headers: {
             'content-type': 'text/plain',
