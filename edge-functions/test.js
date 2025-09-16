851eae9c4df9b406f643f186406eb9c4a304921b
jesperxu <jesperxu@tencent.com>
2025-09-16 14:51:24 +0800

    test.js JSON

--- edge-functions/test.js
+++ edge-functions/test.js
@@ -2,7 +2,8 @@ export function onRequestPost(context) {
     console.log(context)
     const { request } = context;
     const { url, method } = request;
-    const res = new Response(`${method} ${url} ${context}`, {
+    const ctx = JSON.stringify(context)
+    const res = new Response(`${method} ${url} ${ctx}`, {
         status: 200,
         headers: {
             'content-type': 'text/plain',
