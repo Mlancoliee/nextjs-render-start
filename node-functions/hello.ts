0aed7bc370ad8b37aa40461d853f4cc923ffd4b4
Mlancoliee <132282171+Mlancoliee@users.noreply.github.com>
2025-10-23 19:19:29 +0800

    delete const

--- node-functions/hello.ts
+++ node-functions/hello.ts
@@ -1,6 +1,4 @@
 export const onRequestGet = async (context: unknown): Promise<Response> => {
-  const a = 3
-  a = 4
   return new Response('Hello, World!', {
     status: 200,
     headers: { 'Content-Type': 'text/plain; charset=UTF-8' },
