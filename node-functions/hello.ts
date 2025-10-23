5a5550ffd682d70dc327bfd4ab02e7e5c93d2dff
Mlancoliee <132282171+Mlancoliee@users.noreply.github.com>
2025-10-23 19:16:29 +0800

    modify const
    Fix variable reassignment error in onRequestGet.

--- node-functions/hello.ts
+++ node-functions/hello.ts
@@ -1,4 +1,6 @@
 export const onRequestGet = async (context: unknown): Promise<Response> => {
+  const a = 3
+  a = 4
   return new Response('Hello, World!', {
     status: 200,
     headers: { 'Content-Type': 'text/plain; charset=UTF-8' },
